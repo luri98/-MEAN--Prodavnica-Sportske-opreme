a60f720bc2c09350194bb420b44bd1e50d76e14f
Luka Milaš <luka@blackdot.co.me>
2024-07-04 10:06:44 +0200

    UI Navbar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './components/partials/header/header.component';
-
+import { RouterOutlet, RouterModule } from '@angular/router';
+import { NavbarComponent } from './components/partials/navbar/navbar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, RouterModule, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

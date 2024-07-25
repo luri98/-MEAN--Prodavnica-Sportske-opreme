150e8feba61dac62408ad381eaeb95d589544c1f
Luka Milaš <luka@blackdot.co.me>
2024-07-25 16:07:02 +0200

    Completed UI Hero Section

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet, RouterModule } from '@angular/router';
 import { NavbarComponent } from './components/partials/navbar/navbar.component';
 import { FooterComponent } from './components/partials/footer/footer.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, RouterModule, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'prodavnica_sportske_opreme';
--- frontend/src/app/components/partials/hero/hero.component.ts
+++ frontend/src/app/components/partials/hero/hero.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, AfterViewInit, PLATFORM_ID, Inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { BehaviorSubject } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.css'
+  styleUrl: './hero.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class HeroComponent {
+export class HeroComponent implements AfterViewInit {
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
+  
+  private isVisibleSource = new BehaviorSubject<Boolean>(false);
+  isVisible = this.isVisibleSource.asObservable();
 
+  updateVisible(newVal: Boolean) {
+    this.isVisibleSource.next(newVal);
+  }
+
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      setTimeout(() => {
+        this.updateVisible(true)
+      }, 50)
+      // console.log('mounted: ', document.querySelector('swiper-container'));
+    }
+  }
 }
